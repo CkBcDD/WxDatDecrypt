5715d80ae763fca9ddd95bedaa272c64f6ef5fdf
CkBcDD <quqijiangbing@gmail.com>
2025-10-22 15:13:05 +0800

    MOD: using dynamic versioning

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,12 @@
 """backend package"""
 
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
 from . import src
 
 __all__ = ["src"]
